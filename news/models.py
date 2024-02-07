dd1a15539e12307318c235ccf80400cb98d8b864
SrizanKhadka <khadkasrijan785@gmail.com>
2024-02-07 20:50:17 +0545

    serializers in django.

--- news/models.py
+++ news/models.py
@@ -2,15 +2,23 @@ from django.db import models
 
 # Create your models here.
 
+
 class Article(models.Model):
     author = models.CharField(max_length=100)
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=100)
     main_text = models.TextField()
     published_date = models.DateField()
-    is_active = models.BooleanField()
-    created_date = models.DateTimeField(auto_now_add=True) #When this option is set, the field will be set to the current date and time when an object is created for the first time.
-    #Once set during the creation, it won't change subsequently when the object is updated.
+    is_active = models.BooleanField(default=True)
+    created_date = models.DateTimeField(
+        auto_now_add=True
+    )  # When this option is set, the field will be set to the current date and time when an object is created for the first time.
+    # Once set during the creation, it won't change subsequently when the object is updated.
 
-    updated_date = models.DateField(auto_now=True) #When this option is set, the field will be updated to the current date and time every time the object is saved
-    #This means that every time you call save() on the model instance, the updated_date field will be updated.
+    updated_date = models.DateField(
+        auto_now=True
+    )  # When this option is set, the field will be updated to the current date and time every time the object is saved
+    # This means that every time you call save() on the model instance, the updated_date field will be updated.
+
+    def __str__(self):
+        return self.title
---
+++ news/serializer.py
@@ -0,0 +1,27 @@
+from rest_framework import serializers
+from news.models import Article
+
+class ArticleSerializer(serializers.Serializer):
+    id = serializers.IntegerField(readonly=True)
+    author = serializers.CharField()
+    title = serializers.CharField()
+    description = serializers.CharField()
+    main_text = serializers.CharField()
+    published_date = serializers.DateField()
+    is_active = serializers.BooleanField()
+    created_date = serializers.DateTimeField(readonly=True)
+    updated_date = serializers.DateTimeField(readonly=True)
+    
+    def create(self, validated_data):
+        return Article.objects.create(**validated_data)
+    #As we know validated_data is dictonary, to extract it we used double **.
+    
+    def update(self, validated_data,instance):
+        instance.author = validated_data.get('author',instance.author)
+        instance.title = validated_data.get('title', instance.title)
+        instance.description = validated_data.get('description',instance.description)
+        instance.main_text = validated_data.get('main_text', instance.main_text)
+        instance.published_date = validated_data.get('published_date',instance.published_date)
+        instance.is_active = validated_data.get('is_active',instance.is_active)
+        instance.save()
+        return instance
